1b62a4b3962fa9f33ca11d71854303efdd68d5d0
范凯 <fankai@fankaideMacBook-Pro.local>
2019-05-23 23:08:07 +0800

    ..

--- app/src/main/java/com/bella/myview1_1/view/MyVeiw1.java
+++ app/src/main/java/com/bella/myview1_1/view/MyVeiw1.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -59,10 +60,31 @@ public class MyVeiw1 extends View {
         //画直线  由于直线不是封闭图形，所以setStyle(style)对直线没有影响
         canvas.drawLine(100, 820, 300, 850, paint);
         //画很多线
-        float[] lines={100, 880, 300, 880, 100, 980, 300, 980, 200, 880, 200, 980};
-        canvas.drawLines(lines,0, 12, paint);
+        float[] lines = {100, 880, 300, 880, 100, 980, 300, 980, 200, 880, 200, 980};
+        canvas.drawLines(lines, 0, 12, paint);
         //画圆角矩形
         canvas.drawRoundRect(100, 1000, 300, 1100, 60, 60, paint);
+        //画扇形 弧形  为什么画出来的样子跟想象中的样子不一样呢？？？？？？
+        canvas.drawArc(100, 1200, 300, 1800, 0, 360, true, paint);
+
+        //用path画路径图形  这里也画了一个圆  add****
+        Path path = new Path();
+        path.addCircle(500, 100, 100, Path.Direction.CW);
+        canvas.drawPath(path, paint);
+
+        // ***to 画辅助线等
+        canvas.drawLine(500,100, 600, 200, paint);
+        paint.setStyle(Paint.Style.STROKE);  //画线要用STROKE
+        path.lineTo(300, 400);
+        canvas.drawPath(path, paint);
+
+        //移动到目标位置 改变当前位置 moveto
+        //很重要的方法！！！！！
+        path.moveTo(100, 0);
+        path.lineTo(100, 100);
+        canvas.drawPath(path, paint);
+
+
 
 
     }
